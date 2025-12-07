73e70ffa450c6400c62feda0b6aab9312f12b0c8
Add852 <xadd852x@gmail.com>
2025-12-07 12:56:02 +0800

    rofi, syncthing, users.nix, auto maintenance

--- users.nix
+++ users.nix
@@ -1,25 +1,24 @@
-{ ... }:
+{ inputs, config, pkgs, ... }:
 {
   # Define a user account. Don't forget to set a password with ‘passwd’.
     users.users.tony = {
         isNormalUser = true;
         description = "Anthony";
         extraGroups = [ "networkmanager" "wheel" ];
-        packages = with pkgs; [];
     };
 
     # GREETD LOG IN
     services.greetd = { #someone's log in manager online I saw lol (replaced sddm)
         enable = true;
         settings = { # 'rec' is important here to allow recursive reference
-        initial_session = {
-            command = "${pkgs.hyprland}/bin/Hyprland & --cmd noctalia-shell";
-            user = "tony";
-        };
-        default_session = {
-            command = "${pkgs.tuigreet}/bin/tuigreet --greeting 'Welcome to NixOS!' --time --remember --remember-user-session --user-menu --user-menu-min-uid 1000 --asterisks --power-shutdown 'shutdown -P now' --power-reboot 'shutdown -r now' --sessions ${config.services.displayManager.sessionData.desktops}/share/wayland-sessions";
-            user = "greeter";
-        };
+            initial_session = {
+                command = "${pkgs.hyprland}/bin/Hyprland & --cmd noctalia-shell";
+                user = "tony";
+            };
+            default_session = {
+                command = "${pkgs.tuigreet}/bin/tuigreet --greeting 'Welcome to NixOS!' --time --remember --remember-user-session --user-menu --user-menu-min-uid 1000 --asterisks --power-shutdown 'shutdown -P now' --power-reboot 'shutdown -r now' --sessions ${config.services.displayManager.sessionData.desktops}/share/wayland-sessions";
+                user = "greeter";
+            };
         };
     };
 
